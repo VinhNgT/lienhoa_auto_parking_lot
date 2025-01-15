238d2650eddb3d794e4584b19af706e0ad54d3a5
VinhNgT <victorpublic0000@gmail.com>
2025-01-15 17:19:19 +0700

    feat: status lights

--- fastapi_app/base_module/leds.py
+++ fastapi_app/base_module/leds.py
@@ -1,9 +1,11 @@
-# ruff: noqa: F401
-
+import os
+import sys
 import time
 
 from adafruit_extended_bus import ExtendedI2C as I2C
 from adafruit_pcf8574 import PCF8574
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 class LedsPcf8574:
@@ -11,7 +13,13 @@ class LedsPcf8574:
     # too much current flowing to the PCF8574. Try to increase the resistor
     # value or reduce the number of LEDs running at the same time.
 
-    def __init__(self, i2c, address=0x20, led_count=8, reverse_layout=False):
+    def __init__(
+        self,
+        i2c,
+        address=0x20,
+        led_count=8,
+        reverse_layout=False,
+    ):
         if led_count < 1 or led_count > 8:
             raise ValueError("led_count must be between 1 and 8")
 
@@ -46,17 +54,22 @@ class LedsPcf8574:
             not state,
         )
 
-    def set_leds_byte(self, byte: int):
-        if byte < 1 or byte > self.max_byte:
+    def set_leds_byte(self, byte: int, reverse_byte=True):
+        if byte < 0 or byte > self.max_byte:
             raise ValueError(
-                f"byte must be between 0x01 and {hex(self.max_byte)}"
+                f"byte must be between 0x00 and {hex(self.max_byte)}"
             )
 
         # Flip bits because LEDs are active low.
         byte = ~byte & 0xFF
-        self.pcf.write_gpio(
-            byte if not self.reverse_layout else self._reverse_byte(byte)
-        )
+
+        if reverse_byte:
+            byte = self._reverse_bits(byte, self.led_count)
+
+        if self.reverse_layout:
+            byte = self._reverse_bits(byte, 8)
+
+        self.pcf.write_gpio(byte)
 
     @property
     def max_byte(self):
@@ -66,10 +79,10 @@ class LedsPcf8574:
         """
         return (1 << self.led_count) - 1
 
-    def _reverse_byte(self, n):
+    def _reverse_bits(self, n, length):
         result = 0
 
-        for _ in range(8):
+        for _ in range(length):
             # Create space for the next bit.
             result <<= 1
             # Add the rightmost bit of n to result.
@@ -80,14 +93,22 @@ class LedsPcf8574:
         return result
 
 
-with LedsPcf8574(I2C(7), reverse_layout=True, led_count=5) as led_ctl:
-    print("Turning on LEDs.")
+def main():
+    with LedsPcf8574(I2C(7), reverse_layout=True, led_count=4) as led_ctl:
+        print("Turning on LEDs.")
 
-    while True:
-        for i in range(led_ctl.led_count):
-            led_ctl.set_led(i, True)
+        while True:
+            # for i in range(led_ctl.led_count):
+            #     led_ctl.set_led(i, True)
+            #     time.sleep(0.5)
+            #     led_ctl.set_led(i, False)
+
+            # led_ctl.set_leds_byte(0b00001011)
+            led_ctl.set_leds_byte(0b1000)
             time.sleep(0.5)
-            led_ctl.set_led(i, False)
+            led_ctl.set_leds_byte(0)
+            time.sleep(0.5)
 
-    # led_ctl.set_leds_byte(0b00011011)
-    # time.sleep(1)
+
+if __name__ == "__main__":
+    main()
--- fastapi_app/modules/status_lights.py
+++ fastapi_app/modules/status_lights.py
@@ -1,10 +1,18 @@
+import asyncio
 from enum import Enum
 from typing import Annotated
 
+from adafruit_extended_bus import ExtendedI2C as I2C
 from fastapi import APIRouter, Form
 from pydantic import BaseModel, Field
 
-from fastapi_app.gpio_modules import LedsI2c
+from fastapi_app.base_module import LedsPcf8574
+from fastapi_app.utils import RunOnShutdown
+
+leds = LedsPcf8574(I2C(7), reverse_layout=True, led_count=4)
+RunOnShutdown.add(leds.close)
+
+leds_request_lock = asyncio.Lock()
 
 
 class StatusLightState(str, Enum):
@@ -13,31 +21,6 @@ class StatusLightState(str, Enum):
     PROCESSING = "processing"
     ALLOW = "allow"
     DENY = "deny"
-
-
-class StatusLights:
-    def __init__(self):
-        self._leds = LedsI2c(i2c_bus=1, led_count=4)
-        self.set_status(StatusLightState.NONE)
-
-    def set_status(self, state: StatusLightState):
-        match state:
-            case StatusLightState.NONE:
-                self._leds.set_leds_bits(0b0000)
-
-            case StatusLightState.READY:
-                self._leds.set_leds_bits(0b0001)
-
-            case StatusLightState.PROCESSING:
-                self._leds.set_leds_bits(0b0010)
-
-            case StatusLightState.ALLOW:
-                self._leds.set_leds_bits(0b0100)
-
-            case StatusLightState.DENY:
-                self._leds.set_leds_bits(0b1000)
-
-        self.current_state = state
 
 
 class StatusLightsStateResponse(BaseModel):
@@ -51,16 +34,15 @@ router = APIRouter(
     prefix="/status_lights_state",
     tags=["status_lights_state (module PCF8574)"],
 )
-status_lights = StatusLights()
 
 
-@router.get(
-    "/",
-    summary="Get status lights state",
-    response_model=StatusLightsStateResponse,
-)
-def read_status_light():
-    return StatusLightsStateResponse(state=status_lights.current_state)
+# @router.get(
+#     "/",
+#     summary="Get status lights state",
+#     response_model=StatusLightsStateResponse,
+# )
+# def read_status_light():
+#     return StatusLightsStateResponse(state=status_lights.current_state)
 
 
 @router.patch(
@@ -68,6 +50,19 @@ def read_status_light():
     summary="Set status lights state",
     response_model=StatusLightsStateResponse,
 )
-def set_status_light(state: Annotated[StatusLightState, Form()]):
-    status_lights.set_status(state)
-    return StatusLightsStateResponse(state=status_lights.current_state)
+async def set_status_light(state: Annotated[StatusLightState, Form()]):
+    async with leds_request_lock:
+        if state == StatusLightState.NONE:
+            leds.set_leds_byte(0b0000)
+        elif state == StatusLightState.READY:
+            leds.set_leds_byte(0b1000)
+        elif state == StatusLightState.PROCESSING:
+            leds.set_leds_byte(0b0100)
+        elif state == StatusLightState.ALLOW:
+            leds.set_leds_byte(0b0010)
+        elif state == StatusLightState.DENY:
+            leds.set_leds_byte(0b0001)
+        else:
+            raise ValueError(f"Invalid state: {state}")
+
+    return StatusLightsStateResponse(state=state)
